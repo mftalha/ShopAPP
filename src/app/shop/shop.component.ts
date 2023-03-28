501ae0cdac25f9492647c2dc132d7c29423aa1b0
Talha Satır <imnever0729@gmail.com>
2023-03-29 01:48:25 +0300

    feat - ders-10 : Sayfalama

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -13,6 +13,10 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class ShopComponent{
     public selectedCategory: Category | undefined = null!; // list grupta class'a active özelliği vermek için.
+    public productsPerPage = 4; // sayfalarda kaç ürün olacak
+    public selectedPage = 1; //hangi sayfa etkin. = ürün gösterilen sayfa
+    // 1 * 3 => 3 (0,3)
+    // 2 * 3 => 6 (5,3)
 
     constructor(
         private productRepository : ProductRepository,
@@ -20,7 +24,14 @@ export class ShopComponent{
         ) {}
 
     get products(): Product[]{
-        return this.productRepository.getProducts(this.selectedCategory);
+        let index = (this.selectedPage-1) * this.productsPerPage;
+        // sayfada 3'er ürün gösterilmesini istediğimde.
+        // 1. sayfada = (1-1) * 3 => 0. veriden başlıyarak al
+        // 2. sayfada = (2-1) * 3 => 3. veriden başlıyarak al
+
+        return this.productRepository
+        .getProducts(this.selectedCategory)
+        .slice(index,index + this.productsPerPage); //(hangi indexden başlıyacak , kaçıncı veriye kadar alacak) // 0,3 => [0 , 1 , 2] ;; 3,6 => [3 , 4 , 5]
     }
 
     get categories(): Category[]{
