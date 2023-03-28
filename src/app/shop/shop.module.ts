462636a53e44bc7301f2d8084575142ed5907348
Talha Satır <imnever0729@gmail.com>
2023-03-28 03:42:08 +0300

    feat - ders-6 : shop modüllü içerisinde navbar oluşturuldu.

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,13 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { ModelModule } from '../model/model.module';
 import { ShopComponent } from './shop.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 //modül sayfalarında olması şart.
 @NgModule({
     //ModelModule dediğimde artık : Model Module'nin tüm class larınını shop modül içinde kullabıp : api işlemlerimi gerçekleştirebileceğim.
     imports: [ModelModule, BrowserModule, FormsModule], 
     providers:[],
-    declarations: [ShopComponent],
+    declarations: [ShopComponent, NavbarComponent],
     exports: [ShopComponent] //eğerki modülde component var ise : exports etmeliyiz : farklı modülden erişilmesini istiyor isek. 
     // componnet yok ise direk modülden modülü import edip kullanabiliyoruz : kullanılacak modülün export edilmesine gerek yok
 })
