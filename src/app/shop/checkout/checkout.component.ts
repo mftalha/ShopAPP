ce6610f0c9cddc38c7fa20157287ea59b7faf428
Talha Satır <imnever0729@gmail.com>
2023-03-31 04:56:19 +0300

    feat - lesson-18 Order registration

--- src/app/shop/checkout/checkout.component.ts
+++ src/app/shop/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Order } from 'src/app/model/order.model';
+import { OrderRepository } from 'src/app/model/order.repository';
 
 @Component({
   selector: 'checkout',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
 })
 export class CheckoutComponent {
 
+  orderSent: boolean = false;
+  submitted: boolean = false;
+
+  constructor(public order: Order, private orderRepository: OrderRepository) {}
+
+  submitOrder(form: NgForm) {
+    this.submitted = true;
+
+    // bize gelecek form NgForm
+    if(form.valid){ //true ise == kullanıcı tüm required gerekliliklerini yerine getirmiştir.
+      this.orderRepository.saveOrder(this.order)
+      .subscribe(order =>{
+        // kullanıcının tekrar'dan yeni sipariş verebilmesi için hazırlık.
+        this.order.clearOrder(); // kullanıcının kart bilgileri silinecek. 
+        this.orderSent = true;
+        this.submitted = false;
+      })
+    }
+  }
 }
