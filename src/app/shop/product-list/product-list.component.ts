1f93cfa108438f3bb74241058a2439b161599108
Talha Satır <imnever0729@gmail.com>
2023-04-04 03:50:25 +0300

    feat- lesson-21 : Ürün detayının gösterilmesi.

--- src/app/shop/product-list/product-list.component.ts
+++ src/app/shop/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ import { Product } from 'src/app/model/product.model';
 
 export class ProductListComponent {
   @Input() products: Product[] = [];
+  selectedProduct: Product = null as any;
 
   constructor(
     private cart: Cart,
@@ -21,5 +22,13 @@ export class ProductListComponent {
     addProductToCart(product: Product){
       this.cart.addItem(product);
       this.router.navigateByUrl('/cart'); //ekle butonuna tıklayınca detay sayfasına yönlen.
+  }
+
+  displayDetails(product: Product){
+    this.selectedProduct = product;
+  }
+
+  hideDetails(){
+    this.selectedProduct = null as any;
   }
 }
