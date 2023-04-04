9f7921d4fd03d1c34ec081ef540c2ce8eafa10a0
Talha Satır <imnever0729@gmail.com>
2023-04-04 05:16:33 +0300

    feat- lesson-23 : Auth Component

--- src/app/admin/auth/auth.component.ts
+++ src/app/admin/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AuthComponent {
 
+  public username!: string;
+  public password!: string;
+  public errorMessage!: string;
+
+  constructor(private router: Router){
+
+  }
+
+  //NgModül kullanduığımız için modül içine form modülü import etmeliyiz.
+  login(form: NgForm){
+    if(form.valid){
+      if(this.username==='admin' && this.password==='12345'){
+        this.router.navigateByUrl('/admin/main');
+      }else{
+        this.errorMessage = 'Hatalı username ya da parola'
+      }
+    }else{
+      this.errorMessage = 'Bilgileri eksiksiz girin';
+    }
+  }
 }
