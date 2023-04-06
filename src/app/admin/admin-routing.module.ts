af5596d45daeea04353eb100735394d8678bb9f3
Talha Satır <imnever0729@gmail.com>
2023-04-06 05:51:38 +0300

    feat- lesson-28 : Admin Pages

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -4,10 +4,30 @@ import { AuthComponent } from './auth/auth.component';
 import { AdminComponent } from './admin.component';
 import { AuthGuard } from './auth.guard';
 
+import { OrderListComponent } from './orders/order-list/order-list.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
+
+import { CategoryListComponent } from './categories/category-list/category-list.component';
+import { CategoryFormComponent } from './categories/category-form/category-form.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+
+
 const routes: Routes = [
   {path: 'auth', component: AuthComponent},
   //{path: 'main', component: AdminComponent}, //Auth Guards özelliğinden önce böyle idi
-  {path: 'main', component: AdminComponent, canActivate:[AuthGuard]},
+  //{path: 'main', component: AdminComponent, canActivate:[AuthGuard]},
+  {path: 'main', component: AdminComponent, canActivate:[AuthGuard],
+children: [
+  //:mode = biz 'create' mi yoksa 'edit' mi biz ne yazarsak o gelecek ; :id = biz hangi id yi yazar isek o gelecek ==> değişken değer yazmak için.
+  {path: 'products/:mode/:id', component: ProductFormComponent},
+  {path: 'products/:mode/', component: ProductFormComponent}, // id si olmayan Form gelmesi.
+  {path: 'products', component: ProductListComponent}, // id si olmayan Form gelmesi.
+  {path: 'categories/:mode/:id', component: CategoryListComponent},
+  {path: 'categories/:mode/', component: CategoryFormComponent}, // id si olmayan Form gelmesi.
+  {path: 'categories', component: CategoryListComponent}, // id si olmayan Form gelmesi.
+  {path: 'orders', component: OrderListComponent}
+
+]},
   {path: '**', redirectTo: 'auth'} //yukarıdakiler hariç bir url girilirse : path'i auth'a yönlendir.
 ];
 
